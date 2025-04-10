f96641cb129336766bc1391651ffc9f192ae75cd
Lingesh <lingesh@jivrus.com>
2025-04-10 12:17:27 +0530

    Initial setup and project scaffolding for Trip Flow

---
+++ src/app/trip-flow/trip-flow.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-flow',
+  templateUrl: './trip-flow.component.html',
+  styleUrls: ['./trip-flow.component.scss']
+})
+export class TripFlowComponent {
+
+}
